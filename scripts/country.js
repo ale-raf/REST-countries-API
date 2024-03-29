4f273f07b3f97a7247dad41ea58a429f515ae458
Ale Raf <raffinalexis@gmail.com>
2024-03-29 18:31:31 +0100

    script country

---
+++ scripts/country.js
@@ -0,0 +1,68 @@
+import { getJSONData, toggleThemeMode, retrieveTheme } from "./utils.js";
+
+const content = document.querySelector(".country__content");
+const flagElt = document.querySelector(".container img");
+const nameElt = document.querySelector(".container h2");
+const bordersElt = document.querySelector(".country__borders");
+const darkModeBtn = document.querySelector(".header__dark-mode");
+const backBtn = document.querySelector(".back-btn");
+
+let params = new URLSearchParams(document.location.search);
+let id = params.get("id");
+let countries = await getJSONData();
+let country = await getJSONData(id);
+
+darkModeBtn.addEventListener("click", toggleThemeMode);
+
+retrieveTheme(darkModeBtn.firstElementChild);
+
+backBtn.addEventListener("click", () => history.back());
+
+const getBorderCountriesInfo = (border) => {
+  return countries.filter((country) => country.alpha3Code == border)[0];
+};
+
+const showBorderCountries = (border) => {
+  let link = document.createElement("a");
+  link.href = `country.html?id=${getBorderCountriesInfo(border).population}`;
+  link.textContent = getBorderCountriesInfo(border).name;
+  bordersElt.append(link);
+};
+
+const getCountryInfo = (country) => {
+  let keys = [
+    ["nativeName", "Native Name: "],
+    ["population", "Population: "],
+    ["region", "Region: "],
+    ["subregion", "Subregion: "],
+    ["capital", "Capital: "],
+    ["topLevelDomain", "Top Level Domain: "],
+    ["currencies", "Currencies: "],
+    ["languages", "Languages: "],
+  ];
+  for (let i = 0; i < keys.length; i++) {
+    let p = document.createElement("p");
+    let key = keys[i][0];
+    let text = keys[i][1];
+    if (country[key]) {
+      p.textContent = text + country[key]?.toLocaleString("en-US");
+      if (Array.isArray(country[key])) {
+        p.textContent =
+          text + country[key].map((prop) => prop.name ?? prop).join(", ");
+      }
+      content.appendChild(p);
+    }
+  }
+};
+
+const showCountry = (country) => {
+  document.title = "Countries - " + country.name;
+  flagElt.src = country.flag;
+  nameElt.textContent = country.name;
+  getCountryInfo(country);
+  country.borders
+    ? country.borders.map(showBorderCountries)
+    : (bordersElt.innerHTML = "");
+};
+
+showCountry(country);
--- utils.js
+++
@@ -1,6 +0,0 @@
-export async function getJSONData(id) {
-  const res = await fetch("./data.json");
-  const data = await res.json();
-  let dataWithId = data.filter((data) => data.population == id)[0];
-  return id ? dataWithId : data;
-}
